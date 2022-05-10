8fccf2da770d1799fddb7188dd74ccfccc24131f
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-05-09 19:27:51 -0500

    Creating a new board named 'New Board Created Using TrelloJS'

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ require("dotenv").config();
 if(!process.env.TOKEN && !process.env.KEY) {
     throw new Error("No hay configuración con API KEY y con TOKEN");
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let boardTitle = "New Board Created Using TrelloJS";
+
+trello.addBoard(boardTitle, process.env.TOKEN, function (error, trelloCard) {
+    if (error) {
+        console.log('Could not add board:', error);
+    }
+    else {
+        console.log('Added board:', trelloCard);
+    }
+});
