bda656b82447d2469d1205dc65d44746d272ae69
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-05-09 20:30:44 -0500

    Creating new list named 'New List Created Using TrelloJS'

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let Trello = require("trello");
 let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
 let boardTitle = "New Board Created Using TrelloJS";
+let listTitle = "New List Created Using TrelloJS";
 
 trello.addBoard(boardTitle, process.env.TOKEN, function (error, trelloCard) {
     if (error) {
@@ -15,5 +16,14 @@ trello.addBoard(boardTitle, process.env.TOKEN, function (error, trelloCard) {
     }
     else {
         console.log('Added board:', trelloCard);
+    }
+});
+
+trello.addListToBoard("6279ad9710f679178518bbae", listTitle, function (error, trelloList) {
+    if (error) {
+        console.log('Could not create a new list:', error);
+    }
+    else {
+        console.log('Added list:', trelloList);
     }
 });
